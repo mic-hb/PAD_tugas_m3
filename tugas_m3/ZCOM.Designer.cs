87d71f279389cd7b2e2559f27d293afcc98fa31e
mic-hb <michael.h22@mhs.istts.ac.id>
2023-10-25 14:24:16 +0700

    Adding map elements

--- tugas_m3/ZCOM.Designer.cs
+++ tugas_m3/ZCOM.Designer.cs
@@ -35,11 +35,60 @@
             this.labelPoin = new System.Windows.Forms.Label();
             this.txtStatus = new System.Windows.Forms.TextBox();
             this.btnPause = new System.Windows.Forms.Button();
+            this.labelPlayer = new System.Windows.Forms.Label();
+            this.barrier16 = new System.Windows.Forms.Label();
+            this.barrier11 = new System.Windows.Forms.Label();
+            this.barrier1 = new System.Windows.Forms.Label();
+            this.barrier3 = new System.Windows.Forms.Label();
+            this.barrier8 = new System.Windows.Forms.Label();
+            this.barrier12 = new System.Windows.Forms.Label();
+            this.barrier9 = new System.Windows.Forms.Label();
+            this.barrier13 = new System.Windows.Forms.Label();
+            this.barrier14 = new System.Windows.Forms.Label();
+            this.barrier15 = new System.Windows.Forms.Label();
+            this.barrier4 = new System.Windows.Forms.Label();
+            this.barrier2 = new System.Windows.Forms.Label();
+            this.barrier5 = new System.Windows.Forms.Label();
+            this.barrier6 = new System.Windows.Forms.Label();
+            this.barrier7 = new System.Windows.Forms.Label();
+            this.barrier10 = new System.Windows.Forms.Label();
+            this.boxDoor1 = new System.Windows.Forms.Label();
+            this.boxShop = new System.Windows.Forms.Label();
+            this.boxSafeZone = new System.Windows.Forms.Label();
+            this.boxDoor2 = new System.Windows.Forms.Label();
+            this.boxDoor3 = new System.Windows.Forms.Label();
+            this.boxExit = new System.Windows.Forms.Label();
+            this.boxKey = new System.Windows.Forms.Label();
+            this.panelMap.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMap
             // 
             this.panelMap.BackColor = System.Drawing.Color.Black;
+            this.panelMap.Controls.Add(this.boxKey);
+            this.panelMap.Controls.Add(this.boxExit);
+            this.panelMap.Controls.Add(this.boxDoor3);
+            this.panelMap.Controls.Add(this.boxDoor2);
+            this.panelMap.Controls.Add(this.boxSafeZone);
+            this.panelMap.Controls.Add(this.boxShop);
+            this.panelMap.Controls.Add(this.boxDoor1);
+            this.panelMap.Controls.Add(this.barrier10);
+            this.panelMap.Controls.Add(this.barrier7);
+            this.panelMap.Controls.Add(this.barrier6);
+            this.panelMap.Controls.Add(this.barrier5);
+            this.panelMap.Controls.Add(this.barrier2);
+            this.panelMap.Controls.Add(this.barrier4);
+            this.panelMap.Controls.Add(this.barrier15);
+            this.panelMap.Controls.Add(this.barrier14);
+            this.panelMap.Controls.Add(this.barrier13);
+            this.panelMap.Controls.Add(this.barrier9);
+            this.panelMap.Controls.Add(this.barrier12);
+            this.panelMap.Controls.Add(this.barrier8);
+            this.panelMap.Controls.Add(this.barrier3);
+            this.panelMap.Controls.Add(this.barrier1);
+            this.panelMap.Controls.Add(this.barrier11);
+            this.panelMap.Controls.Add(this.barrier16);
+            this.panelMap.Controls.Add(this.labelPlayer);
             this.panelMap.Location = new System.Drawing.Point(0, 0);
             this.panelMap.Name = "panelMap";
             this.panelMap.Size = new System.Drawing.Size(750, 500);
@@ -103,6 +152,214 @@
             this.btnPause.Text = "Pause";
             this.btnPause.UseVisualStyleBackColor = true;
             // 
+            // labelPlayer
+            // 
+            this.labelPlayer.BackColor = System.Drawing.Color.Gray;
+            this.labelPlayer.Location = new System.Drawing.Point(1, 449);
+            this.labelPlayer.Name = "labelPlayer";
+            this.labelPlayer.Size = new System.Drawing.Size(50, 50);
+            this.labelPlayer.TabIndex = 0;
+            this.labelPlayer.Text = "P";
+            this.labelPlayer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // barrier16
+            // 
+            this.barrier16.BackColor = System.Drawing.Color.White;
+            this.barrier16.Location = new System.Drawing.Point(65, 427);
+            this.barrier16.Name = "barrier16";
+            this.barrier16.Size = new System.Drawing.Size(621, 10);
+            this.barrier16.TabIndex = 1;
+            // 
+            // barrier11
+            // 
+            this.barrier11.BackColor = System.Drawing.Color.White;
+            this.barrier11.Location = new System.Drawing.Point(65, 231);
+            this.barrier11.Name = "barrier11";
+            this.barrier11.Size = new System.Drawing.Size(10, 196);
+            this.barrier11.TabIndex = 2;
+            // 
+            // barrier1
+            // 
+            this.barrier1.BackColor = System.Drawing.Color.White;
+            this.barrier1.Location = new System.Drawing.Point(65, 62);
+            this.barrier1.Name = "barrier1";
+            this.barrier1.Size = new System.Drawing.Size(260, 10);
+            this.barrier1.TabIndex = 3;
+            // 
+            // barrier3
+            // 
+            this.barrier3.BackColor = System.Drawing.Color.White;
+            this.barrier3.Location = new System.Drawing.Point(137, 62);
+            this.barrier3.Name = "barrier3";
+            this.barrier3.Size = new System.Drawing.Size(10, 172);
+            this.barrier3.TabIndex = 4;
+            // 
+            // barrier8
+            // 
+            this.barrier8.BackColor = System.Drawing.Color.White;
+            this.barrier8.Location = new System.Drawing.Point(210, 137);
+            this.barrier8.Name = "barrier8";
+            this.barrier8.Size = new System.Drawing.Size(10, 237);
+            this.barrier8.TabIndex = 5;
+            // 
+            // barrier12
+            // 
+            this.barrier12.BackColor = System.Drawing.Color.White;
+            this.barrier12.Location = new System.Drawing.Point(283, 225);
+            this.barrier12.Name = "barrier12";
+            this.barrier12.Size = new System.Drawing.Size(10, 202);
+            this.barrier12.TabIndex = 6;
+            // 
+            // barrier9
+            // 
+            this.barrier9.BackColor = System.Drawing.Color.White;
+            this.barrier9.Location = new System.Drawing.Point(213, 220);
+            this.barrier9.Name = "barrier9";
+            this.barrier9.Size = new System.Drawing.Size(464, 10);
+            this.barrier9.TabIndex = 7;
+            // 
+            // barrier13
+            // 
+            this.barrier13.BackColor = System.Drawing.Color.White;
+            this.barrier13.Location = new System.Drawing.Point(676, 225);
+            this.barrier13.Name = "barrier13";
+            this.barrier13.Size = new System.Drawing.Size(10, 144);
+            this.barrier13.TabIndex = 8;
+            // 
+            // barrier14
+            // 
+            this.barrier14.BackColor = System.Drawing.Color.White;
+            this.barrier14.Location = new System.Drawing.Point(286, 290);
+            this.barrier14.Name = "barrier14";
+            this.barrier14.Size = new System.Drawing.Size(319, 10);
+            this.barrier14.TabIndex = 9;
+            // 
+            // barrier15
+            // 
+            this.barrier15.BackColor = System.Drawing.Color.White;
+            this.barrier15.Location = new System.Drawing.Point(355, 359);
+            this.barrier15.Name = "barrier15";
+            this.barrier15.Size = new System.Drawing.Size(331, 10);
+            this.barrier15.TabIndex = 10;
+            // 
+            // barrier4
+            // 
+            this.barrier4.BackColor = System.Drawing.Color.White;
+            this.barrier4.Location = new System.Drawing.Point(387, 62);
+            this.barrier4.Name = "barrier4";
+            this.barrier4.Size = new System.Drawing.Size(10, 168);
+            this.barrier4.TabIndex = 11;
+            // 
+            // barrier2
+            // 
+            this.barrier2.BackColor = System.Drawing.Color.White;
+            this.barrier2.Location = new System.Drawing.Point(387, 62);
+            this.barrier2.Name = "barrier2";
+            this.barrier2.Size = new System.Drawing.Size(299, 10);
+            this.barrier2.TabIndex = 12;
+            // 
+            // barrier5
+            // 
+            this.barrier5.BackColor = System.Drawing.Color.White;
+            this.barrier5.Location = new System.Drawing.Point(460, 62);
+            this.barrier5.Name = "barrier5";
+            this.barrier5.Size = new System.Drawing.Size(10, 96);
+            this.barrier5.TabIndex = 13;
+            // 
+            // barrier6
+            // 
+            this.barrier6.BackColor = System.Drawing.Color.White;
+            this.barrier6.Location = new System.Drawing.Point(531, 135);
+            this.barrier6.Name = "barrier6";
+            this.barrier6.Size = new System.Drawing.Size(10, 85);
+            this.barrier6.TabIndex = 14;
+            // 
+            // barrier7
+            // 
+            this.barrier7.BackColor = System.Drawing.Color.White;
+            this.barrier7.Location = new System.Drawing.Point(601, 71);
+            this.barrier7.Name = "barrier7";
+            this.barrier7.Size = new System.Drawing.Size(10, 96);
+            this.barrier7.TabIndex = 15;
+            // 
+            // barrier10
+            // 
+            this.barrier10.BackColor = System.Drawing.Color.White;
+            this.barrier10.Location = new System.Drawing.Point(676, 135);
+            this.barrier10.Name = "barrier10";
+            this.barrier10.Size = new System.Drawing.Size(10, 90);
+            this.barrier10.TabIndex = 16;
+            // 
+            // boxDoor1
+            // 
+            this.boxDoor1.BackColor = System.Drawing.Color.Silver;
+            this.boxDoor1.Location = new System.Drawing.Point(65, 72);
+            this.boxDoor1.Name = "boxDoor1";
+            this.boxDoor1.Size = new System.Drawing.Size(72, 72);
+            this.boxDoor1.TabIndex = 17;
+            this.boxDoor1.Text = "DOOR";
+            this.boxDoor1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // boxShop
+            // 
+            this.boxShop.BackColor = System.Drawing.Color.Silver;
+            this.boxShop.Location = new System.Drawing.Point(75, 355);
+            this.boxShop.Name = "boxShop";
+            this.boxShop.Size = new System.Drawing.Size(72, 73);
+            this.boxShop.TabIndex = 18;
+            this.boxShop.Text = "SHOP";
+            this.boxShop.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // boxSafeZone
+            // 
+            this.boxSafeZone.BackColor = System.Drawing.Color.Silver;
+            this.boxSafeZone.Location = new System.Drawing.Point(220, 137);
+            this.boxSafeZone.Name = "boxSafeZone";
+            this.boxSafeZone.Size = new System.Drawing.Size(167, 83);
+            this.boxSafeZone.TabIndex = 18;
+            this.boxSafeZone.Text = "SAFE ZONE";
+            this.boxSafeZone.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // boxDoor2
+            // 
+            this.boxDoor2.BackColor = System.Drawing.Color.Silver;
+            this.boxDoor2.Location = new System.Drawing.Point(470, 72);
+            this.boxDoor2.Name = "boxDoor2";
+            this.boxDoor2.Size = new System.Drawing.Size(59, 59);
+            this.boxDoor2.TabIndex = 19;
+            this.boxDoor2.Text = "DOOR";
+            this.boxDoor2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // boxDoor3
+            // 
+            this.boxDoor3.BackColor = System.Drawing.Color.Silver;
+            this.boxDoor3.Location = new System.Drawing.Point(617, 230);
+            this.boxDoor3.Name = "boxDoor3";
+            this.boxDoor3.Size = new System.Drawing.Size(59, 59);
+            this.boxDoor3.TabIndex = 20;
+            this.boxDoor3.Text = "DOOR";
+            this.boxDoor3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // boxExit
+            // 
+            this.boxExit.BackColor = System.Drawing.Color.Silver;
+            this.boxExit.Location = new System.Drawing.Point(308, 237);
+            this.boxExit.Name = "boxExit";
+            this.boxExit.Size = new System.Drawing.Size(45, 45);
+            this.boxExit.TabIndex = 21;
+            this.boxExit.Text = "EXIT";
+            this.boxExit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // boxKey
+            // 
+            this.boxKey.BackColor = System.Drawing.Color.Silver;
+            this.boxKey.Location = new System.Drawing.Point(406, 84);
+            this.boxKey.Name = "boxKey";
+            this.boxKey.Size = new System.Drawing.Size(45, 24);
+            this.boxKey.TabIndex = 22;
+            this.boxKey.Text = "KEY";
+            this.boxKey.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // ZCOM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -118,6 +375,7 @@
             this.Name = "ZCOM";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ZCOM";
+            this.panelMap.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,6 +390,30 @@
         private System.Windows.Forms.Label labelPoin;
         private System.Windows.Forms.TextBox txtStatus;
         private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Label labelPlayer;
+        private System.Windows.Forms.Label barrier16;
+        private System.Windows.Forms.Label barrier11;
+        private System.Windows.Forms.Label barrier3;
+        private System.Windows.Forms.Label barrier1;
+        private System.Windows.Forms.Label barrier12;
+        private System.Windows.Forms.Label barrier8;
+        private System.Windows.Forms.Label barrier9;
+        private System.Windows.Forms.Label barrier15;
+        private System.Windows.Forms.Label barrier14;
+        private System.Windows.Forms.Label barrier13;
+        private System.Windows.Forms.Label barrier4;
+        private System.Windows.Forms.Label barrier5;
+        private System.Windows.Forms.Label barrier2;
+        private System.Windows.Forms.Label barrier10;
+        private System.Windows.Forms.Label barrier7;
+        private System.Windows.Forms.Label barrier6;
+        private System.Windows.Forms.Label boxDoor1;
+        private System.Windows.Forms.Label boxSafeZone;
+        private System.Windows.Forms.Label boxShop;
+        private System.Windows.Forms.Label boxDoor2;
+        private System.Windows.Forms.Label boxDoor3;
+        private System.Windows.Forms.Label boxExit;
+        private System.Windows.Forms.Label boxKey;
     }
 }
 
