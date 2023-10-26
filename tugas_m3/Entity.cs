3d2da87daa86fca5cbd0c349cb38deb6ebf228af
mic-hb <michael.h22@mhs.istts.ac.id>
2023-10-26 19:18:37 +0700

    Adding entities classes

---
+++ tugas_m3/Entity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace tugas_m3
+{
+    internal class Entity
+    {
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Entity(string name, int x, int y)
+        {
+            this.Name = name;
+            this.X = x;
+            this.Y = y;
+        }
+
+        public Entity()
+        {
+            this.Name = null;
+            this.X = 0;
+            this.Y = 0;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X += x;
+            this.Y += y;
+        }
+    }
+}
---
+++ tugas_m3/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_m3
+{
+    internal class Player : Entity
+    {
+        public Player(string name, int x, int y) : base(name, x, y)
+        {
+        }
+
+        public Player() : base()
+        {
+        }
+    }
+}
